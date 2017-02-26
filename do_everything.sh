1781b4667a0265cabc52bf053bb81fa87344d2ff
lorenmh <neutronstar@gmail.com>
2017-02-25 20:16:32 -0800

    upd

---
+++ do_everything.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+cd ${0%/*}/files
+
+set -e
+
+source /usr/local/sbin/setup.sh cdh-5.6
+
+for i in $(seq -f "%02g" 0 49)
+do
+  fname="posts-2016-0000000000$i"
+  jsname="$fname.json"
+  gzname="$jsname.gz"
+  upname="posts_2016_$i.json"
+  curl -O 'https://storage.googleapis.com/coen281-posts-2016-gz/'$fname'.json.gz'
+  gunzip $gzname
+  hadoop fs -copyFromLocal $jsname $upname
+  rm $jsname
+done
