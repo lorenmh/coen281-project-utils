53124dd700481e3ce1637aceaad51815e50673b6
lorenmh <neutronstar@gmail.com>
2017-02-25 21:13:50 -0800

    accumulate into single file

---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+cd ${0%/*}/files
+
+set -e
+
+hfile='posts_all.json'
+
+for i in $(seq -f "%02g" 0 49)
+do
+  fname="posts-2016-0000000000$i.json.gz"
+  url="https://storage.googleapis.com/coen281-posts-2016-gz/$fname"
+  echo "on file $hfile"
+  curl $url | gunzip -c >> $hfile
+done
+
+source /usr/local/sbin/setup.sh cdh-5.6
+hadoop fs -copyFromLocal $hfile $hfile
