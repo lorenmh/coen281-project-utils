d29a17641beab07e12d9886d7c8b193d1de3b925
lorenmh <neutronstar@gmail.com>
2017-02-25 19:59:22 -0800

    added status msg

--- unzip_files.sh
+++ unzip_files.sh
@@ -3,5 +3,7 @@ cd ${0%/*}/files
 
 for i in $(seq -f "%02g" 0 1)
 do
-  gunzip 'posts-2016-0000000000'$i'.json.gz'
+  fname='posts-2016-0000000000'$i'.json.gz'
+  echo "unzipping $fname"
+  gunzip $fname
 done
